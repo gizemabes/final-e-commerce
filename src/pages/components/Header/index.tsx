d176a64b18a5430f5b21d23c69b41ae16696a73a
Sevval <yildizsevval659@gmail.com>
2024-01-10 21:53:53 +0300

    update

--- src/pages/components/Header/index.tsx
+++ src/pages/components/Header/index.tsx
@@ -3,18 +3,33 @@ import Image from "next/image"
 import {FiUser} from 'react-icons/Fi'
 import { useRouter } from "next/router";
 import { Button } from 'primereact/button';
+import { SlBasket } from "react-icons/sl";
+import { LuHeart } from "react-icons/lu";
 
+const onClick = () => {
+  // Sepet tıklandığında yapılacak işlemler
+  console.log('Sepet tıklandı!');}
 
+  
 const Header = () => {
 const {push, pathname} = useRouter()
 const [isLoggedIn, setIsLoggedIn] = useState(false)
+const [basketCount] = useState(0);
 
-console.log(pathname)
 return (
-        <div style={{backgroundColor: 'white',display: "flex" ,flexDirection: 'row', padding: 14, justifyContent: 'space-between'}}>
-            <Image width={150} height={150} alt="logo" src='/images/logo.png'/>
-            <div style={{display: "flex", flexDirection: 'row', padding: 3  ,justifyContent: 'space-around', alignItems :'center'}}>
-            <FiUser size={25} color={'#349590'}/>
+  <div style={{ backgroundColor: 'white', display: 'flex', flexDirection: 'row', padding: 14, justifyContent: 'space-between' }}>
+  <Image width={150} height={150} alt="logo" src='/images/logo.png' />
+  <div style={{ display: 'flex', flexDirection: 'row', padding: 10, justifyContent: 'space-around', alignItems: 'center' }}>
+    <LuHeart size={25} color={'#349590'} />
+    <div onClick={onClick} style={{ display: 'flex', flexDirection: 'row', padding: 10, justifyContent: 'space-between', alignItems: 'center' , cursor: 'pointer' }}>
+      <SlBasket size={25} color={'#349590'} />
+      <span style={{ position: 'relative', top: '-15px', left: '-10px', display: 'flex', width: '22px', height: '22px', justifyContent: 'center', alignItems: 'center', borderRadius: '11px', fontSize: '14px', background: 'red', color: 'white' }}>{basketCount}</span>
+
+
+    </div>
+    <div style={{ display: 'flex', flexDirection: 'row', padding: 2, justifyContent: 'flex-start', alignItems: 'center' }}>
+      <FiUser size={25} color={'#349590'} />
+    </div>
 
 {
     pathname !== '/login' && (
@@ -30,17 +45,24 @@ return (
           {/* Conditionally render the user's name button */}
          { isLoggedIn && (
             <Button 
-              label={`hahahah`} 
+              label={hahahah} 
               onClick={() => push('')}
               style={{ backgroundColor: 'white', color: '#349590', padding: 8 }}
             />
           )}
+          
+        
         </div>
+        
       )
+      
+      
 }
             </div>
         </div>
     )
 }
 
-export default Header
+
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,83 +1,128 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Layout from "@/pages/components/Layout";
-import Product from "@/pages/components/Product";
-
-const inter = Inter({ subsets: ['latin'] })
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Layout from "@/pages/components/Layout/index";
+import Product from './components/Product/index';
 
 const data = [
   {
     name: 'urun',
     price: 25,
-    image: '/images/urun.png',
+    image: '/images/1.png',
     stokc: 333
   },
   {
     name: 'urun2',
     price: 25,
-    image: '/images/urun.png',
+    image: '/images/2.png',
     stokc: 46
   },
   {
     name: 'urun3',
     price: 25,
-    image: '/images/urun.png',
+    image: '/images/3.png',
     stokc: 600
   },
   {
     name: 'urun',
     price: 25,
-    image: '/images/urun.png',
+    image: '/images/11.png',
     stokc: 333
   },
   {
     name: 'urun2',
     price: 25,
-    image: '/images/urun.png',
+    image: '/images/22.png',
     stokc: 46
   },
   {
     name: 'urun3',
     price: 25,
-    image: '/images/urun.png',
+    image: '/images/33.png',
     stokc: 600
   },
   {
     name: 'urun2',
     price: 25,
-    image: '/images/urun.png',
+    image: '/images/44.png',
     stokc: 46
   },
   {
     name: 'urun3',
     price: 25,
-    image: '/images/urun.png',
+    image: '/images/55.png',
     stokc: 600
   },
+];
 
-
-]
-
-export default function Home() {
+const Home = () => {
   const addToCart = () => {
+    // Sepete ekleme işlemi burada gerçekleştirilebilir.
+  };
 
-  }
+  const mainSliderSettings = {
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 500,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      
+    ],
+
+    variableWidth: true,
+    variableHeight:false,
+  };
+
+  const secondarySliderSettings = {
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+  };
+
   return (
-      <Layout >
+    <Layout>
+      <div style={{ backgroundColor: '#f5f5f5', padding:'80px' }}>
+      <Slider {...secondarySliderSettings} className="second-slider">
+                <div>
+                  <img src="/images/slider123.jpg" alt="Example 1" style={{ width: '100%', height: '700px' }} />
+                </div>
+                <div>
+                  <img src="/images/slider5.jpeg" alt="Example 2" style={{ width: '100%', height: '800px' }} />
+                </div>
+              </Slider>
+        <Slider {...mainSliderSettings}>
+          {data.map((item, key) => (
+            <div key={key} style={{ margin: 20, backgroundColor: '#fff', position: 'relative'}}>
+              <Product
+                name={item.name}
+                price={item.price}
+                stock={item.stokc}
+                image={item.image}
+                onClick={addToCart}
+              />
+              
+              
+            </div>
+          ))}
+        </Slider>
+       
+      </div>
+    </Layout>
+  );
+};
 
-        <div style={{ backgroundColor: '#f5f5f5',padding:'80px' }}>
-          <div style={{ display: "flex", margin: 20, backgroundColor: '#fff', flexDirection: 'row', flexWrap: 'wrap', justifyContent: "space-around" }}>
-
-            {
-              data.map((item, key) => (
-                  <Product key={key} name={item.name} price={item.price} stock={item.stokc} image={item.image} onClick={addToCart} />
-              ))
-            }
-
-          </div>
-
-        </div>
-
-      </Layout>
-  )
-}
+export default Home;
